2db3c975610e26b365d4db0afc6bf4d757ae5e39
shreyasjaiwal <97226633+shreyasjaiwal@users.noreply.github.com>
2023-01-18 16:39:14 +0530

    Add files via upload

---
+++ QR_Scanner/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import QRscanner from './components/QrScanner';
+function App() {
+  return (
+    <div className="App">
+      <h1>QR Scanner</h1>
+      <h2>Hello World</h2>
+      <QRscanner/>
+    </div>
+  );
+}
+
+export default App;
